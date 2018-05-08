53bf65abb8adec6efd8b3ca00b23a7ab3adb583c
indestinee Chen <indestinee@gmail.com>
2018-05-08 19:51:40 +0800

    baseline

--- main.py
+++ main.py
@@ -1,56 +1,63 @@
 import requests, os, getpass, pickle, time
 from urllib.parse import quote
-from IPython import embed
 
-sess = requests.Session()
+def main():
+    sess = requests.Session()
+    ret = ''
+    while True:
+        host = 'http://210.77.16.21/'
+        response = sess.get(host, allow_redirects=False)
 
-while True:
-    host = 'http://210.77.16.21/'
-    response = sess.get(host, allow_redirects=False)
+        logout_url = response.headers['location']
+        response = sess.get(logout_url, allow_redirects=False)
 
-    logout_url = response.headers['location']
-    response = sess.get(logout_url, allow_redirects=False)
+        login_information = response.headers['location']
 
-    login_information = response.headers['location']
-# print(login_information)
+        def information():# {{{
+            if not os.path.isdir('./data'):
+                os.mkdir('./data')
+            if os.path.isfile('./data/user'):
+                with open('./data/user', 'rb') as f:
+                    return pickle.load(f)
+            username = input('username: ')
+            passwd = getpass.getpass('password: ')    
+            with open('./data/user', 'wb') as f:
+                pickle.dump([username, passwd], f)
+            return username, passwd
+        # }}}
 
-    def information():# {{{
-        if not os.path.isdir('./data'):
-            os.mkdir('./data')
-        if os.path.isfile('./data/user'):
-            with open('./data/user', 'rb') as f:
-                return pickle.load(f)
-        username = input('username: ')
-        passwd = getpass.getpass('password: ')    
-        with open('./data/user', 'wb') as f:
-            pickle.dump([username, passwd], f)
-        return username, passwd
-# }}}
-    username, passwd = information()
-    cookies = login_information[login_information.find('?')+1:]
+        username, passwd = information()
+        cookies = login_information[login_information.find('?')+1:]
 
-    if cookies[:9] == 'userIndex':
-        userIndex = cookies[10:]
-        logout = 'http://210.77.16.21/eportal/InterFace.do?method=logout'
-        sess.post(logout, {'userIndex': userIndex})
-        print('[LOG] logout')
-        time.sleep(2)
-    else:
-        break
+        if cookies[:9] == 'userIndex':
+            userIndex = cookies[10:]
+            logout = 'http://210.77.16.21/eportal/InterFace.do?method=logout'
+            sess.post(logout, {'userIndex': userIndex})
+            print('[LOG] logout')
+            ret += '[LOG] logout<br>'
+            time.sleep(2)
+        else:
+            break
 
-data = {# {{{
-    'userId': quote(username),
-    'password': passwd,
-    'service': '',
-    'queryString': cookies,
-    'operatorPwd': '',
-    'operatorUserId': '',
-    'validcode': '',
-    'passwordEncrypt': 'false',
-}# }}}
+    data = {# {{{
+        'userId': quote(username),
+        'password': passwd,
+        'service': '',
+        'queryString': cookies,
+        'operatorPwd': '',
+        'operatorUserId': '',
+        'validcode': '',
+        'passwordEncrypt': 'false',
+    }# }}}
 
-login = 'http://210.77.16.21/eportal/InterFace.do?method=login'
-response = sess.post(login, data=data)
-response.encoding = 'utf-8'
+    login = 'http://210.77.16.21/eportal/InterFace.do?method=login'
+    response = sess.post(login, data=data)
+    response.encoding = 'utf-8'
 
-print(response.json()['result'])
+    print('[LOG] server return', response.json()['result'])
+    ret += '[LOG] server return' + response.json()['result'] + '<br>'
+    return ret
+
+if __name__ == '__main__':
+    main()
+
